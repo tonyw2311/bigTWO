dc8856062d600f6d6669df081bacf9853cb88aa1
Anthony <theanthonywong@tamu.edu>
2023-08-27 13:35:33 -0500

    trial socket server test

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,39 @@
 import adapter from '@sveltejs/adapter-node';
-import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-
 	kit: {
 		adapter: adapter()
 	}
 };
 
 export default config;
+
+/* import adapter from '@sveltejs/adapter-node';
+import injectSocketIO from './socketIoHandler.js';
+
+// @type {import('@sveltejs/kit').Config} 
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+
+	kit: {
+		adapter: adapter()
+	},
+
+	vite: {
+		plugins: [
+			{
+				name: "sveltekit-socket-io",
+				configureServer(server) {
+					injectSocketIO(server.httpServer); // <- call the function here
+				}
+			}
+		]
+	}
+};
+
+export default config;
+ */
