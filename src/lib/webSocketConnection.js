2efa0ef32b671fe61bc014203129007385d9fcd5
Anthony <theanthonywong@tamu.edu>
2023-08-27 21:33:11 -0500

    prod8

--- src/lib/webSocketConnection.js
+++ src/lib/webSocketConnection.js
@@ -2,6 +2,6 @@ import ioClient from 'socket.io-client';
 
 const ENDPOINT = import.meta.env.VITE_UNSECURE_SECRET;
 
-const socket = ioClient(ENDPOINT);
+const socket = ioClient("https://bigtwo.pages.dev");
 
 export const io = socket;
