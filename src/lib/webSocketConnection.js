b9c50ec3269252b4a1b165fcad122a548e40bc83
Anthony <theanthonywong@tamu.edu>
2023-08-27 22:09:44 -0500

    prod13

--- src/lib/webSocketConnection.js
+++ src/lib/webSocketConnection.js
@@ -2,6 +2,6 @@ import ioClient from 'socket.io-client';
 
 const ENDPOINT = import.meta.env.VITE_UNSECURE_SECRET;
 
-const socket = ioClient('https://bigtwo.pages.dev:8443', { transports: ['websocket', 'polling', 'flashsocket'] });
+const socket = ioClient('wss://bigtwo.pages.dev:8443', { transports: ['websocket', 'polling', 'flashsocket'] });
 
 export const io = socket;
